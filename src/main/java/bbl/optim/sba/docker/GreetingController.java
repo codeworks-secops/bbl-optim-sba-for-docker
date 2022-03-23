ce769d415fcba6f2bf2ae8908fe07b0490d686af
Hamza <hamza.el-yaaqoubi-ext@yrnet.com>
2022-03-23 17:52:59 +0100

    change greeting message

--- src/main/java/bbl/optim/sba/docker/GreetingController.java
+++ src/main/java/bbl/optim/sba/docker/GreetingController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
+public class GreetingController {
 
     @RequestMapping("/")
     public String hello() {
-        return "Hello Everybody !!!!!!!!";
+        return "Hello Everybody !!";
     }
 
     @RequestMapping("/{name}")
